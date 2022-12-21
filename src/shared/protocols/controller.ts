fa808af3f7f479cb5c827ee9cdb42c5279bfdce2
Bruno Meneses <brunomenesessousa@gmail.com>
2022-12-21 15:07:50 -0300

    feat: add http-helper, controller and files

--- src/shared/protocols/controller.ts
+++ src/shared/protocols/controller.ts
@@ -4,6 +4,6 @@ import { HttpRequest, HttpResponse } from "./http";
 export interface Controller {
   handle: (
     request?: HttpRequest,
-    response?: HttpResponse
+    response?: HttpResponse,
   ) => Promise<HttpResponse>;
 }
